4cb3a43d8fcdfaefef5ebbb97d7994086eb4d83d
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-01-13 17:01:22 +0100

    -

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 const verify = require("../routes/verifyToken");
 const client = require("../database");
 const jwt = require("jsonwebtoken");
+const uuid = require("uuid");
+
+router.get("/my-posts", verify, async (req, res) => {
+    const decoded = jwt.verify(req.headers.token, process.env.TOKEN_SECRET); 
+    const userId = decoded._id;
+
+    if(!userId){
+        return res.status(400).send("couldn't find user");
+    }
+
+    const data = await client.query(`select * from blogs where userid = '${userId}'`);
+    res.send(data.rows);
+})
+
 
 router.post("/post", verify, async (req, res) => {
     const decoded = jwt.verify(req.headers.token, process.env.TOKEN_SECRET); 
@@ -16,8 +30,18 @@ router.post("/post", verify, async (req, res) => {
         return res.status(400).send("invalid input");
     }
 
-    const data = await client.query(`insert into blogs(title, content, userId) values ('${blog.title}', '${blog.content}', '${blog.userId}')`);
+    const data = await client.query(`insert into blogs(title, content, userId) values ('${blog.title}', '${blog.content}', '${blog.userId}', ${uuid.v4()})`);
     res.send("created blog");
 })
+
+router.delete("/delete/:blogId", verify, (req, res) => {
+    const blogId = req.params.blogId;
+
+    if(!blog.params.blogId){
+        return res.status(400).send("invalid blog id");
+    }
+})
+
+
 
 module.exports = router;
